9974e8b030eb6f0f477f316d2b154d29fb68826d
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-14 17:53:13 +0900

    set holiday pull

--- src/_data.ts
+++ src/_data.ts
@@ -4,6 +4,7 @@ export const todaysDateYYYYMMDD = `${new Date().toISOString().split("T")[0]}`;
 // export const todaysDateYYYYMMDD2 = `${new Date().toISOString().slice(0, 10)}`;
 
 export const todaysDateJAJP = `${new Date().toLocaleString('ja-JP',{ year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' })}`;
+export const todaysDateENUS = `${new Date().toLocaleString('en-US',{ year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' })}`;
 
 const response = await fetch('https://holidays-jp.github.io/api/v1/date.json', {
   method: 'GET',
