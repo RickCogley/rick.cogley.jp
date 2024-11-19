fa37d83202e2d7c8b30d988e37a179712cece6bc
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-19 19:13:22 +0900

    Fetch rss from status.lol, display on readme

--- src/_data.ts
+++ src/_data.ts
@@ -1,3 +1,4 @@
+// Simple constants
 export const cacheBuster = `${new Date().getTime()}`;
 export const todaysDateYYYYMMDD = `${new Date().toISOString().split("T")[0]}`;
 
@@ -6,6 +7,7 @@ export const todaysDateYYYYMMDD = `${new Date().toISOString().split("T")[0]}`;
 export const todaysDateJAJP = `${new Date().toLocaleString('ja-JP',{ year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' })}`;
 export const todaysDateENUS = `${new Date().toLocaleString('en-US',{ year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' })}`;
 
+// Fetch holidays
 const response = await fetch('https://holidays-jp.github.io/api/v1/date.json', {
   method: 'GET',
   mode: 'no-cors',
@@ -19,6 +21,7 @@ export {
   holidays,
 };
 
+// Get repo folder size
 import { join } from "https://deno.land/std/path/mod.ts";
 
 async function getFolderSize(path: string): Promise<number> {
@@ -45,6 +48,27 @@ getFolderSize(folderPath).then(size => {
 export const repoSizeLong = await getFolderSize(folderPath);
 export const repoSizeMB = Math.trunc(repoSizeLong/1024/1024);
 
+// Shuffle
 // https://en.wikipedia.org/wiki/Fisher-Yates_shuffle
 import shuffle from "https://deno.land/x/shuffle/mod.ts";
 console.log(shuffle(["i","ro","ha","ni","ho","he","to"]));
+
+// Import rss feed and convert to json
+import { parseFeed } from "https://deno.land/x/rss/mod.ts";
+async function fetchAndConvertRSS(url: string, limit: number) {
+  // Fetch the RSS feed
+  const response = await fetch(url);
+  const xml = await response.text();
+  // Parse the RSS feed
+  const feed = await parseFeed(xml);
+  // Limit the number of entries
+  const limitedEntries = feed.entries.slice(0, limit);
+  // Convert to JSON
+  // const json = JSON.stringify( {...feed, entries: limitedEntries }, null, 2);
+  const json = { ...feed, entries: limitedEntries };
+  return json;
+}
+const rssUrl = "https://rick.status.lol/feed/rss";
+const limit = 5;
+fetchAndConvertRSS(rssUrl,limit).then(console.log).catch(console.error);
+export const statuses = await fetchAndConvertRSS(rssUrl,limit);
